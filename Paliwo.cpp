02854d7ec7f7faee872e56b31f861d6bd483acab
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-13 00:28:15 +0100

    Zadanie Paliwo

---
+++ Paliwo.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	double n, b;
+
+	cin >> n >> b;
+
+	double vat = b - n;
+
+	vat = vat / 2;
+
+	cout << fixed << setprecision(2) << vat << " " << fixed << setprecision(2) << n + vat;
+
+	return 0;
+}
