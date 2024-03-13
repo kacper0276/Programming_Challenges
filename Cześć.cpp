d32d89060eb3c54e68cac7a64826605656963870
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-13 14:05:30 +0100

    Zadanie ze spoja o nazwie "Cześć"

---
+++ Cześć.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	int wynik = 0;
+	int polaczoneOsoby = 0;
+	int l, poprzednia, helpCounter = 1;
+	cin >> n;
+	if (n >= 2 && n <= 10) {
+		cin >> l;
+		if (l >= 1 && l <= 10) {
+			poprzednia = l;
+			for (int i = 1; i < n; i++) {
+				cin >> l;
+				if (l >= 1 && l <= 10) {
+					if (poprzednia == l) {
+						helpCounter++;
+					}
+					else {
+						if (polaczoneOsoby != 0 || helpCounter != 1) {
+							wynik += polaczoneOsoby + helpCounter;	
+						}
+						polaczoneOsoby += helpCounter;
+						poprzednia = l;
+						helpCounter = 1;
+					}
+				}
+			}
+		}
+		wynik += polaczoneOsoby + helpCounter;
+
+		cout << wynik;
+	}
+
+	return 0;
+}
