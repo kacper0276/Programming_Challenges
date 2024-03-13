139ef83d76568c214cc1fcd458266ff8e9b60044
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-13 23:10:18 +0100

    Zadanie ze spoja o nazwie "Ego"

---
+++ Ego.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string ktoKtoryDzien(int n) {
+	if ((n >= 1 && n <= 7) || (n >= 43 && n <= 49)) {
+		return "DAWID";
+	}
+	else if ((n >= 8 && n <= 14) || (n >= 50 && n <= 56)) {
+		return "GRZESIEK";
+	}
+	else if ((n >= 15 && n <= 21) || (n >= 57 && n <= 63)) {
+		return "MACIEK";
+	}
+	else if ((n >= 22 && n <= 28) || (n >= 64 && n <= 70)) {
+		return "MARCIN";
+	}
+	else if ((n >= 29 && n <= 35)) {
+		return "MARIUSZ";
+	}
+	else if ((n >= 36 && n <= 42)) {
+		return "RAFAL";
+	}
+}
+
+int main() {
+	int t, n;
+
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+
+		cout << ktoKtoryDzien(n) << endl;
+	}
+
+	return 0;
+}
