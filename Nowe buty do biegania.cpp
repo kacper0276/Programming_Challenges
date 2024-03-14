2f3c68970ed0a469691a1f5f1b141a7ba5a6de82
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-14 09:20:22 +0100

    Zadanie ze spoja o nazwie "Nowe buty do biegania"

---
+++ Nowe buty do biegania.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	const double mile = 1.609344;
+	int k;
+
+	cin >> k;
+	double wynik = k / mile;
+
+	if (wynik < 300) {
+		cout << "NIE";
+	}
+	else if (wynik >= 300 && wynik < 500) {
+		cout << "SPRAWDZIMY TWOJE OBECNE BUTY";
+	}
+	else {
+		cout << "TAK";
+	}
+
+	return 0;
+}
