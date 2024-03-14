e4e8979d3b1e85f11c387fbb8e112db43d9625d2
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-14 09:12:01 +0100

    Zadanie o nazwie "Policz budynki"

---
+++ Policz budynki.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+
+
+int main() {
+	map<int, string> mapa;
+	int n;
+	int l;
+	string nazwisko;
+
+	cin >> n;
+
+	while (n--) {
+		cin >> l >> nazwisko;
+		mapa[l] = nazwisko;
+	}
+
+	cout << mapa.size();
+
+	return 0;
+}
