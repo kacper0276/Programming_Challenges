8995a958d3291ddfc230348475c5fda8476c6144
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-14 09:05:02 +0100

    Zadanie o nazwie "Snake case na camel case"

---
+++ Snake case na camel case.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	string zdanie, wynik = "";
+	cin >> zdanie;
+	char poprzedni = zdanie[0];
+
+	wynik = zdanie[0];
+
+	for (int i = 1; i < zdanie.length(); i++) {
+		if (poprzedni == '_') {
+			wynik += zdanie[i] - 32;
+		}
+		else if (zdanie[i] != '_') {
+			wynik += zdanie[i];
+		}
+		poprzedni = zdanie[i];
+	}
+	
+	cout << wynik;
+
+	return 0;
+}
