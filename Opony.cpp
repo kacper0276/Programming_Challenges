3fc47d345376661df0f5ccae8296b07582e2e91b
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-14 10:09:49 +0100

    Zadanie ze spoja o nazwie "Opony"

---
+++ Opony.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+	map<string, int> mapa;
+	string napis;
+
+	while (getline(cin, napis))
+	{
+		if (napis.empty()) {
+			break;
+		}
+
+		mapa[napis]++;
+	}
+
+	for (auto& el : mapa) {
+		if (el.second % 2 == 1) {
+			cout << el.first;
+			return 0;
+		}
+	}
+
+	return 0;
+}
