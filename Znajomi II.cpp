3d64b2ad2c1405d22981ecddd8f03e86dc62ac70
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-14 22:36:16 +0100

    Zadanie ze Spoj'a o nazwie "Znajomi II"

---
+++ Znajomi II.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool czyTaRodzina(string wyraz, string rodzina) {
+	sort(wyraz.begin(), wyraz.end());
+	return wyraz == rodzina;
+}
+
+int main() {
+	map<string, int> mapa;
+	vector<string> wyrazy;
+	int max = 0;
+	string rodzina = "";
+	string napis;
+	int n;
+	cin >> n;
+
+	while (n--) {
+		cin >> napis;
+		wyrazy.push_back(napis);
+		sort(napis.begin(), napis.end());
+		mapa[napis]++;
+		if (mapa[napis] > max) {
+			max = mapa[napis];
+			rodzina = napis;
+		}
+	}
+
+	for (auto& el : wyrazy) {
+		if (czyTaRodzina(el, rodzina)) {
+			cout << el << endl;
+		}
+	}
+
+	return 0;
+}
