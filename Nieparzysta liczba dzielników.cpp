127e2f1de2905a3f7414ad9bfa92f1462bb9531d
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-15 10:44:47 +0100

    Zadanie ze Spoj'a o nazwie "Nieparzysta liczba dzielników"

---
+++ Nieparzysta liczba dzielników.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <set>
+
+using namespace std;
+
+bool czyNiejparzystaLiczbaDzielnikow(int n) {
+    set<int> zbior;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            zbior.insert(i);
+            zbior.insert(n / i);
+        }
+    }
+    return zbior.size() % 2 == 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    if (czyNiejparzystaLiczbaDzielnikow(n)) {
+        cout << "TAK" << endl;
+    }
+    else {
+        cout << "NIE" << endl;
+    }
+    return 0;
+}
