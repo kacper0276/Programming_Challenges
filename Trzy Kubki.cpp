41dbb2a50c29e5221a27596f78b30bbdc8a6aad7
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 16:22:23 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Trzy Kubki

---
+++ Trzy Kubki.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int* tab, int l1, int l2) {
+	int help = tab[l1 - 1];
+	tab[l1 - 1] = tab[l2 - 1];
+	tab[l2 - 1] = help;
+}
+
+int main() {
+	int n, q, l1, l2;
+	cin >> n;
+	int* pozycjeKubkow = new int[n];
+	for (int i = 0; i < n; i++) {
+		pozycjeKubkow[i] = i + 1;
+	}
+
+	cin >> q;
+
+	while (q--) {
+		cin >> l1 >> l2;
+		swap(pozycjeKubkow, l1, l2);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << pozycjeKubkow[i] << " ";
+	}
+
+	return 0;
+}
