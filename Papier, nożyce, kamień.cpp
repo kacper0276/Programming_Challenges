93edbfdaccd8e097dc3ccda980b31aa7848c04d5
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 12:34:47 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Papier, nożyce, kamień

---
+++ Papier, nożyce, kamień.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string gra(string napis) {
+	if (napis == "nozyce") {
+		return "kamien";
+	} 
+	else if (napis == "kamien") {
+		return "papier";
+	} 
+	else if (napis == "papier") {
+		return "nozyce";
+	}
+	else {
+		return "przegrales";
+	}
+}
+
+int main() {
+	string wybor;
+
+	cin >> wybor;
+
+	cout << gra(wybor);
+
+	return 0;
+}
