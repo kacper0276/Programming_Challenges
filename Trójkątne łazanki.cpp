5778584f6abeff7806c4afca9446c286a11d6f0a
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 16:00:19 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Trójkątne łazanki

---
+++ Trójkątne łazanki.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool czySaIdentyczne(std::vector<std::vector<int>>& lazanki) {
+    sort(lazanki.begin(), lazanki.end());
+    for (int i = 1; i < lazanki.size(); ++i) {
+        if (lazanki[i] == lazanki[i - 1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> lazanki;
+    for (int i = 0; i < n; ++i) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        vector<int> lazanka = { a, b, c };
+        sort(lazanka.begin(), lazanka.end());
+        lazanki.push_back(lazanka);
+    }
+
+    if (czySaIdentyczne(lazanki)) {
+        cout << "Tak" << endl;
+    }
+    else {
+        cout << "Nie" << endl;
+    }
+
+    return 0;
+}
