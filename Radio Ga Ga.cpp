6e63d3a758ca370be3ac1f5423a51fbd6ad2e0e7
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 14:45:29 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Radio Ga Ga

---
+++ Radio Ga Ga.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	string ostatnia = "", obecna;
+	int n;
+	bool mama = false, exist = false;
+
+	cin >> n;
+
+	cin >> obecna;
+	ostatnia = obecna;
+	n--;
+
+	while (n--) {
+		cin >> obecna;
+
+		if (obecna == "ma" && ostatnia == "ma" && !exist) {
+			mama = true;
+			exist = true;
+		} 
+
+		if (obecna == "ta" && ostatnia == "ta" && !exist) {
+			exist = true;
+		}
+
+		ostatnia = obecna;
+	}
+
+	if (mama) {
+		cout << "mama";
+	}
+	else {
+		cout << "tata";
+	}
+
+	return 0;
+}
