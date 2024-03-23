07790ce2085c52b0affdb149cff55cd584f6d0ae
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 14:38:01 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Cyfry

---
+++ Cyfry.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	string pierwsza = "- 1", ostatnia = "-1";
+	string napis;
+	cin >> t;
+
+	while (t--) {
+		cin >> napis;
+
+		for (auto& el : napis) {
+			if (el >= '0' && el <= '9' && pierwsza == "- 1") {
+				pierwsza = el;
+			}
+			else if (el >= '0' && el <= '9' && pierwsza != "- 1") {
+				ostatnia = el;
+			}
+		}
+
+		if (pierwsza != "- 1" && ostatnia != "- 1") {
+			cout << pierwsza << " " << ostatnia << endl;
+		}
+		else {
+			cout << "BRAK" << endl;
+		}
+
+		pierwsza = "- 1", ostatnia = "- 1";
+	}
+
+	return 0;
+}
