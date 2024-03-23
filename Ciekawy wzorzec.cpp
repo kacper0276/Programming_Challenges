ff3a21f699e0ce68cda1a4dad62937cd57e5c01a
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 15:33:46 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Ciekawy wzorzec

---
+++ Ciekawy wzorzec.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int funkcjaDoZadania(string napis, string wzorzec) {
+	int currentIndex = 0, firstLetter = -1;
+
+	for (int i = 0; i < napis.size(); i++) {
+		if (napis[i] == wzorzec[0]) {
+			currentIndex++;
+			for (int j = i + 2; j < napis.size(); j += 2) {				
+				if (napis[j] == wzorzec[currentIndex]) {
+					currentIndex++;
+
+					if (currentIndex == wzorzec.size()) {
+						return i;
+					}
+				}
+
+				else {
+					currentIndex = 0;
+					break;
+				}
+			}
+		}
+	}
+
+	return -1;
+}
+
+int main() {
+	string napis, wzorzec;
+
+	cin >> napis;
+	cin >> wzorzec;
+
+	int wynik = funkcjaDoZadania(napis, wzorzec);
+
+	if (wynik == -1) {
+		cout << "brak";
+	}
+	else {
+		cout << ++wynik;
+	}
+
+	return 0;
+}
