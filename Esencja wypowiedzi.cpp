3a7ca963a3be13a5f9286e534857c89e5f7f7ccc
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-23 15:06:33 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Esencja wypowiedzi

---
+++ Esencja wypowiedzi.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool czyEmotka(string napis) {
+	return napis == ":)" || napis == ":(" || napis == ";)" || napis == ":D" || napis == ":P" || napis == "xD" || napis == ":>" || napis == "<3";
+}
+
+int main() {
+	int n, m;
+
+	cin >> n;
+	string** tablica = new string* [n];
+	int* rozmiary = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> m;
+		rozmiary[i] = m;
+		tablica[i] = new string[m];
+		for (int j = 0; j < m; j++) {
+			cin >> tablica[i][j];
+		}
+	}
+
+	
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < rozmiary[i]; j++) {
+			if (!czyEmotka(tablica[i][j])) {
+				cout << tablica[i][j] << " ";
+			}
+		}
+		cout << endl;
+	}
+  
+
+	return 0;
+}
