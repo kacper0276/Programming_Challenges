1fd227c257e6fc208dba9563c25c3f97e28c2ce3
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-27 11:53:33 +0100

    Zadania ze spoj'a o nazwie "Macierz NWD"

---
+++ Macierz NWD.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+int NWD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> sequence(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> sequence[i];
+    }
+
+    std::vector<std::vector<int>> gcdMatrix(n, std::vector<int>(n));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            gcdMatrix[i][j] = NWD(sequence[i], sequence[j]);
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << gcdMatrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
