3433fa07e53eb6b0d5724433fe48cb2dddbf1be4
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-27 11:04:21 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Jąkała

---
+++ Jąkała.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool czyLiteraWTablicy(char litera, char* tablica, int k) {
+	for (int i = 0; i < k; i++) {
+		if (tablica[i] == litera)
+			return true;
+	}
+
+	return false;
+}
+
+string jakanieSie(string napis, char* tablica, int k) {
+	string wynik = "";
+	for (char c : napis) {
+		wynik += c;
+		if (czyLiteraWTablicy(c, tablica, k)) {
+			wynik += c;
+		}
+	}
+
+	return wynik;
+}
+
+int main() {
+	int k;
+	string napis;
+	cin >> k;
+
+	if (k >= 1 && k <= 26) {
+		char* tab = new char[k];
+		for (int i = 0; i < k; i++) {
+			cin >> tab[i];
+		}
+		cin >> napis;
+
+		cout << jakanieSie(napis, tab, k);
+	}
+
+	return 0;
+}
