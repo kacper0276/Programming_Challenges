3937ce96e546b764f96c45c8f3c36a44cd1973b9
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-27 11:29:39 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Ropuszka

---
+++ Ropuszka.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, n;
+
+	cin >> a >> b >> n;
+
+	if (a >= 1 && b >= 1 && a <= 30 && b <= 30 && a != b && n >= 1 && n <= 365) {
+		if (n == 33) {
+			cout << "WRACAJ";
+		}
+		else if (a > b) {
+			cout << "W PRAWO";
+		}
+		else {
+			cout << "W LEWO";
+		}
+	}
+
+	return 0;
+}
