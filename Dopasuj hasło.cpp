33f87ed9570ac9349a607d786e848abad1de5817
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-27 10:50:26 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Dopasuj hasło

---
+++ Dopasuj hasło.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool funkcjaDoZadania(string haslo, string zaszyfrowaneHaslo) {
+	int index = 0;
+	for (char c : zaszyfrowaneHaslo) {
+		if (c != '*') {
+			if (index >= haslo.size() || c != haslo[index]) {
+				return false;
+			}
+		}
+			index++;
+	}
+
+	return index == haslo.size();
+}
+
+int main() {
+	int n;
+	string haslo, zaszyfrowaneHaslo;
+	cin >> n;
+
+	while (n--) {
+		cin >> haslo;
+		cin >> zaszyfrowaneHaslo;
+
+		if (funkcjaDoZadania(haslo, zaszyfrowaneHaslo)) {
+			cout << "ok" << endl;
+		}
+		else {
+			cout << "error" << endl;
+		}
+	}
+
+	return 0;
+}
