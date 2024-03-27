ae44a3877d408e59aa2f79d11573350537fd0bad
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-27 10:38:03 +0100

    Zadania ze spoj'a z konkursu Fraktal (23.03.2024) Parzysta parzystość

---
+++ Parzysta parzystość.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool czyParzysta(long long int liczba) {
+	return liczba % 2 == 0;
+}
+
+bool czyPrzestawisz(long long int liczba) {
+	string str = to_string(liczba);
+	sort(str.begin(), str.end());
+
+	do {
+		long long int l = stoll(str);
+		if (l % 2 == 0) {
+			return true;
+		}
+	} while (next_permutation(str.begin(), str.end()));
+
+	return false;
+}
+
+int main() {
+	long long int liczba;
+	cin >> liczba;
+
+	if (czyParzysta(liczba)) {
+		cout << "Tak";
+	}
+	else {
+		if (czyPrzestawisz(liczba)) {
+			cout << "Tak";
+		}
+		else {
+			cout << "Nie";
+		}
+	}
+
+	return 0;
+}
