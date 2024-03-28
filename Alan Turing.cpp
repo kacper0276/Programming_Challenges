b274ba01e16dbd43da38af385861cee20558723d
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-03-28 09:40:27 +0100

    Zadania ze spoj'a o nazwie "Alan Turing"

---
+++ Alan Turing.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string napis = "";
+
+    for (int i = 0; i < n; i++) {
+        string instrukcja;
+        cin >> instrukcja;
+
+        if (instrukcja == "DODAJ") {
+            char litera;
+            cin >> litera;
+            napis += litera;
+        }
+        else if (instrukcja == "USUN") {
+            int liczbaZnakow;
+            cin >> liczbaZnakow;
+            if (liczbaZnakow > napis.length()) {
+                napis = "";
+            }
+            else {
+                napis.erase(napis.length() - liczbaZnakow, liczbaZnakow);
+            }
+        }
+        else if (instrukcja == "ZAMIEN") {
+            char litera;
+            cin >> litera;
+            if (!napis.empty()) {
+                napis[napis.length() - 1] = litera;
+            }
+        }
+    }
+
+    cout << napis << endl;
+
+    return 0;
+}
