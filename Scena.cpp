750785caa8abdaa16f9fd980d20886596899736f
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-03 20:48:08 +0200

    Zadania ze spoj'a o nazwie "Scena"

---
+++ Scena.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	struct Point
+	{
+		int x;
+		int y;
+	};
+
+	int n, wynik = 0;
+	Point jas;
+	cin >> jas.x;
+	cin >> jas.y;
+	cin >> n;
+	while (n--) {
+		Point wspolrzedne;
+		cin >> wspolrzedne.x >> wspolrzedne.y;
+		if ((wspolrzedne.x >= 0 && wspolrzedne.x <= jas.x && wspolrzedne.y >= 0 && wspolrzedne.y <= jas.y) &&
+			(jas.x * wspolrzedne.y == jas.y * wspolrzedne.x)) {
+			wynik++;
+		}
+	}
+
+	cout << wynik;
+	
+	return 0;
+}
