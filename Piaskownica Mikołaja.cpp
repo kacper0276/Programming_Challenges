7a9c633feb5477e2b0b26c2fbab8c9fa5fa0b0af
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-03 11:50:54 +0200

    Zadania ze spoj'a o nazwie "Piaskownica Mikołaja"

---
+++ Piaskownica Mikołaja.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int l1, l2, l3, l4;
+	cin >> l1 >> l2 >> l3 >> l4;
+	int arr[4] = {l1, l2, l3, l4};
+	sort(arr, arr + 4);
+
+	cout << arr[0] * arr[2];
+
+	return 0;
+}
