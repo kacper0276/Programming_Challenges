d6a40634b632d80f32900060da1a787d33d4bfc4
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-03 12:10:57 +0200

    Zadania ze spoj'a o nazwie "Autorzy"

---
+++ Autorzy.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string funkcja(int g, int m) {
+    if (g == m) {
+        string wynik = "2";
+        for (int i = 0; i < g; i++) {
+            wynik += "0";
+        }
+
+        return wynik;
+    }
+    else {
+        string wynik = "1";
+        string mniejsza = "1";
+        bool gWieksza = g > m ? true : false;
+        
+        if (gWieksza) {
+            for (int i = 0; i < g - m - 1; i++) {
+                wynik += "0";
+            }
+
+            for (int i = 0; i < m; i++) {
+                mniejsza += "0";
+            }
+        }
+        else {
+            for (int i = 0; i < m - g - 1; i++) {
+                wynik += "0";
+            }
+
+            for (int i = 0; i < g; i++) {
+                mniejsza += "0";
+            }
+        }
+        wynik += mniejsza;
+        return wynik;
+    }
+}
+
+int main() {
+    int g, m;
+    cin >> g >> m;
+
+    cout << funkcja(g, m);
+
+    return 0;
+}
