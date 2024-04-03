d2b5246d61f3d60f9859b755758cc3f93c65a989
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-03 12:26:49 +0200

    Zadania ze spoj'a o nazwie "Zbieranie drużyny"

---
+++ Zbieranie drużyny.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int czyZagramy(map<int, bool> mapa) {
+	int wynik = 0;
+
+	for (auto& el : mapa) {
+		if (el.second == true) {
+			wynik += 1;
+		}
+	}
+
+	return wynik;
+}
+
+int main() {
+	int n, numer;
+	string wiadomosc;
+	map<int, bool> mapa;
+	cin >> n;
+
+	while (n--) {
+		cin >> numer >> wiadomosc;
+		if (wiadomosc == "bede" || wiadomosc == "ja" || wiadomosc == "gram") {
+			mapa[numer] = true;
+		}
+		else if (wiadomosc == "odpadam" || wiadomosc == "rezygnuje") {
+			mapa[numer] = false;
+		}
+	}
+
+	int wynik = czyZagramy(mapa);
+
+	if (wynik >= 10) {
+		cout << "Gramy! Bedzie nas " << wynik;
+	}
+	else {
+		cout << "Nie gramy :( Jest nas tylko " << wynik;
+	}
+
+	return 0;
+}
