9285c8cf831689e7f898661a846048c2f54b98b4
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-03 12:15:56 +0200

    Zadania ze spoj'a o nazwie "Góry i doliny"

---
+++ Góry i doliny.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, wynik = 0;
+	cin >> n;
+
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i < n - 1; i++) {
+		if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
+			wynik++;
+		}
+	}
+
+	cout << wynik;
+}
