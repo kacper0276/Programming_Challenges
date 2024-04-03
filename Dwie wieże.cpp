7f564f9417c1e6cb1bd984eb60d34a20985775c6
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-03 10:56:53 +0200

    Zadania ze spoj'a o nazwie "Dwie wieże"

---
+++ Dwie wieże.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	string pozycja1, pozycja2;
+
+	cin >> pozycja1 >> pozycja2;
+
+	if (pozycja1[0] != pozycja2[0] && pozycja1[1] != pozycja2[1]) {
+		cout << "NIE";
+	}
+	else {
+		cout << "TAK";
+	}
+
+	return 0;
+}
