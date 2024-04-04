7b96ffd9f50349990e5c554cd1af250463783b90
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-04 12:08:46 +0200

    Zadania ze spoj'a o nazwie "Nawiasowanie"

---
+++ Nawiasowanie.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool isMatchingPair(char opening, char closing) {
+    if (opening == '(' && closing == ')')
+        return true;
+    else if (opening == '[' && closing == ']')
+        return true;
+    else if (opening == '{' && closing == '}')
+        return true;
+    else if (opening == '<' && closing == '>')
+        return true;
+    else
+        return false;
+}
+
+bool isBalanced(const string& expression) {
+    stack<char> stack;
+
+    for (char bracket : expression) {
+        if (bracket == '(' || bracket == '[' || bracket == '{' || bracket == '<') {
+            stack.push(bracket);
+        }
+        else if (bracket == ')' || bracket == ']' || bracket == '}' || bracket == '>') {
+            if (stack.empty() || !isMatchingPair(stack.top(), bracket)) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.empty();
+}
+
+int main() {
+    string expression;
+    cin >> expression;
+
+    if (isBalanced(expression)) {
+        cout << "Tak" << endl;
+    }
+    else {
+        cout << "Nie" << endl;
+    }
+
+    return 0;
+}
