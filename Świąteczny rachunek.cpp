08ac368074f921807535d9efd2f8fc7acb6fd0d0
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-04 11:22:20 +0200

    Zadania ze spoj'a o nazwie "Świąteczny rachunek"

---
+++ Świąteczny rachunek.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ileWydal(string zdanie) {
+	int wynik = 0;
+	string cyfry = "";
+
+	for (int i = 0; i < zdanie.length(); i++) {
+		if (isdigit(zdanie[i])) {
+			cyfry = cyfry + zdanie[i];
+		}
+		else if (cyfry != "") {
+			int liczba = stoi(cyfry);
+			wynik += liczba;
+			cyfry = "";
+		}
+	}
+
+	if (cyfry != "") {
+		int liczba = stoi(cyfry);
+		wynik += liczba;
+	}
+
+	return wynik;
+}
+
+int main() {
+	string zdanie = "";
+	getline(cin, zdanie);
+
+	cout << ileWydal(zdanie);
+
+	return 0;
+}
