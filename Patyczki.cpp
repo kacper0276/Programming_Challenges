e1d4e43d4f01f213ca27ed62731923c9c49859ce
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-04 10:10:39 +0200

    Zadania ze spoj'a o nazwie "Patyczki"

---
+++ Patyczki.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int ileTrojkatowRownobocznych(map<int, int> mapa) {
+	int wynik = 0;
+	for (auto el : mapa) {
+		while (el.second >= 0) {
+			if (el.second >= 3) {
+				wynik++;
+			}
+			el.second -= 3;
+		}
+	}
+
+	return wynik;
+}
+
+int main() {
+	int n, liczba;
+	map<int, int> mapa;
+	cin >> n;
+
+	while (n--) {
+		cin >> liczba;
+		mapa[liczba]++;
+	}
+
+	cout << ileTrojkatowRownobocznych(mapa);
+
+	return 0;
+}
