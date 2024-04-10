53a270a9dfdbc428de3021fbc24257dafa9b69c6
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-10 14:14:11 +0200

    Zadania ze strony Szkopuł o nazwie "Prostopadłościan"

---
+++ Prostopadłościan.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a * b * c << endl;
+	cout << (2 * (c * b)) + (2 * (a * b)) + (2 * (a * c));
+
+	return 0;
+}
