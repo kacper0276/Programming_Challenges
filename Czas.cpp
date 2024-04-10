f53763fafc6cd269f4883433b127a0e29d685c83
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-10 12:49:30 +0200

    Zadania ze strony Szkopuł o nazwie "Czas"

---
+++ Czas.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void funkcjaDoZadania(int t) {
+	int godziny = 0, minuty = 0, sekundy = 0;
+
+	godziny = t / 3600;
+	t = t % 3600;
+	minuty = t / 60;
+	t = t % 60;
+	sekundy = t;
+
+	cout << godziny << "g" << minuty << "m" << sekundy << "s";
+}
+
+int main() {
+	int t;
+	cin >> t;
+	funkcjaDoZadania(t);
+
+	return 0;
+}
