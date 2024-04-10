559ec26225ba8c9a695f104f339a9861a7ec89d5
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-10 12:38:10 +0200

    Zadania ze strony Szkopuł o nazwie "Choinka"

---
+++ Choinka.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int spacje = 4;
+	int gwiazdki = 1;
+	int pom = 2;
+
+	while (pom--)
+	{
+		while (spacje > 0) {
+			for (int i = 0; i < spacje; i++) {
+				cout << " ";
+			}
+			for (int i = 0; i < gwiazdki; i++) {
+				cout << "*";
+			}
+			cout << endl;
+			gwiazdki += 2;
+			spacje -= 1;
+		}
+
+		spacje = 4;
+		gwiazdki = 1;
+	}
+	for (int i = 0; i < 9; i++) {
+		cout << "*";
+	}
+
+	return 0;
+}
