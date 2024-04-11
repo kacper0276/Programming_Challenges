87498d25b7546d664dc506cef24357f0b7fb3680
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-11 08:04:55 +0200

    Zadania ze strony Szkopuł o nazwie "Koszykarz"

---
+++ Koszykarz.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	// wzrost obecny, wymagany, o ile wzrasta
+	int k, w, m;
+	cin >> k >> w >> m;
+	
+	int roznica = w - k;
+	int uderzenia = (int)ceil((double)roznica / m);
+
+	cout << uderzenia;
+
+	return 0;
+}
