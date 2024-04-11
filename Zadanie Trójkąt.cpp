49c476c88080ce4dc16c02e6fa86c085e7b021d0
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-11 08:35:16 +0200

    Zadania ze strony Szkopuł o nazwie "Zadanie Trójkąt"

---
+++ Zadanie Trójkąt.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void czyMoznaZbudowac(int32_t a, int32_t b, int32_t c) {
+	if (a > b && a > c) {
+		if ((b + c) > a) {
+			cout << "TAK";
+		}
+		else {
+			cout << "NIE";
+		}
+	}
+	else if (b > a && b > c) {
+		if ((a + c) > b) {
+			cout << "TAK";
+		}
+		else {
+			cout << "NIE";
+		}
+	}
+	else {
+		if ((b + a) > c) {
+			cout << "TAK";
+		}
+		else {
+			cout << "NIE";
+		}
+	}
+}
+
+int main() {
+	int32_t a, b, c;
+	cin >> a >> b >> c;
+
+	czyMoznaZbudowac(a, b, c);
+
+	return 0;
+}
