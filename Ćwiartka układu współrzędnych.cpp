b6c5d4d00f4263c76d47cdcd145bd5f5673532d8
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-11 08:28:21 +0200

    Zadania ze strony Szkopuł o nazwie "Ćwiartka układu współrzędnych"

---
+++ Ćwiartka układu współrzędnych.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	// zmienna int ktora na pewno ma 32 bity
+	int32_t x, y;
+
+	cin >> x >> y;
+
+	if (x == 0 && y == 0) {
+		cout << "0";
+	}
+	else if (x == 0 && y != 0) {
+		cout << "OY";
+	}
+	else if (x != 0 && y == 0) {
+		cout << "OX";
+	}
+	else {
+		if (x > 0 && y > 0) {
+			cout << "I";
+		}
+		else if (x < 0 && y > 0) {
+			cout << "II";
+		}
+		else if (x < 0 && y < 0) {
+			cout << "III";
+		}
+		else if (x > 0 && y < 0) {
+			cout << "IV";
+		}
+	}
+
+	return 0;
+}
