4ddb792cddfe33c3943a1c40822772d8578567aa
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-11 07:54:21 +0200

    Zadania ze strony Szkopuł o nazwie "Potęgi dwójki"

---
+++ Potęgi dwójki.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+unsigned long long int pow(unsigned long long int n) {
+	unsigned long long int liczba = 1;
+	
+	if (n == 0)
+		return liczba;
+
+	for (int i = 0; i < n; i++) {
+		liczba *= 2;
+	}
+
+	return liczba;
+}
+
+void printPow2UntilN(unsigned long long int n) {
+	unsigned long long int i = 0;
+	
+	while (pow(i) <= n)
+	{
+		if (pow(i) <= n) {
+			cout << pow(i) << endl;
+			i++;
+		}
+	}
+}
+
+int main() {
+	unsigned long long int n;
+	cin >> n;
+
+	printPow2UntilN(n);
+	return 0;
+}
