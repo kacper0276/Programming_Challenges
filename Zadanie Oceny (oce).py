6afbdfa5fee30a29d72f9a2a8b6438c9730de996
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-04-17 10:58:31 +0200

    Zadania ze strony Szkopuł o nazwie "Zadanie Oceny (oce)"

---
+++ Zadanie Oceny (oce).py
@@ -0,0 +1,6 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+wyniki = [arr.count(1), arr.count(2), arr.count(3), arr.count(4), arr.count(5), arr.count(6)]
+
+print(wyniki[0], wyniki[1], wyniki[2], wyniki[3], wyniki[4], wyniki[5])
