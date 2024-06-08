bb0e48fa687fb63da7ccd032f55a03ff7cffaa34
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-06-09 00:26:42 +0200

    Codewars import C# file

---
+++ Codewars/C#/Two Oldest Ages/Program.cs
@@ -0,0 +1,11 @@
+﻿public class LargestTwo
+{
+    public static int[] TwoOldestAges(int[] ages) => ages.OrderBy(x => x).TakeLast(2).ToArray();
+
+    static void Main()
+    {
+        int[] res = TwoOldestAges([1, 2]);
+
+        Console.WriteLine("{0} {1}", res[0], res[1]);
+    }
+}
