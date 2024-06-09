829ac73c04d0fa4bb2ef47f243de22f6bf934dc1
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-06-09 19:58:06 +0200

    Exercise Spot the Differences [C#]

---
+++ Codewars/C#/Spot the Differences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+    public static List<int> Spot(string s1, string s2)
+    {
+        List<int> list = new();
+        
+        for (int i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                list.Add(i);
+            }
+        }
+
+        return list;
+    }
+
+    static void Main()
+    {
+        List<int> res = Spot("Hello World!", "hello world.");
+
+        foreach (int i in res)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
