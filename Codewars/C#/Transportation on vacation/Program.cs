7551c8c8d7dad614b3b37fdfce58a75b4562904c
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-06-09 09:35:40 +0200

    exercise Transportation on vacation [C#]

---
+++ Codewars/C#/Transportation on vacation/Program.cs
@@ -0,0 +1,29 @@
+﻿public class RentalCar
+{
+
+    public static int RentalCarCost(int d)
+    {
+        int cost = d * 40;
+
+        switch(d)
+        {
+            case >= 7:
+                cost -= 50;
+                break;
+            case >= 3:
+                cost -= 20;
+                break;
+            default:
+                break;
+        }
+
+        return cost;
+    }
+
+    static void Main(String[] args)
+    {
+        Console.WriteLine(RentalCarCost(1));
+        Console.WriteLine(RentalCarCost(3));
+        Console.WriteLine(RentalCarCost(7));
+    }
+}
