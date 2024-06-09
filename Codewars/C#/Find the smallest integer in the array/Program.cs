d2c59e9f66e80fb66e5ac0fd64e2fca8dcab6dd6
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2024-06-09 09:52:57 +0200

    Exercise Find the smallest integer in the array

---
+++ Codewars/C#/Find the smallest integer in the array/Program.cs
@@ -0,0 +1,22 @@
+﻿    public class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            int min = args[0];
+
+            foreach (int arg in args)
+            { 
+                if (arg < min)
+                {
+                    min = arg;
+                }
+            }
+
+            return min;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(FindSmallestInt(new int[] { 78, 56, -2, 12, 8, -33 }));
+        }
+    }
