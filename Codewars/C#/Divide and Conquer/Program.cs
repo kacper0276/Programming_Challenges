ed365d0cc038b6953af7c3ff7ba14048eff6039a
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-06-13 12:54:06 +0200

    Exercise Divide and Conquer [C#]

---
+++ Codewars/C#/Divide and Conquer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int DivCon(Object[] objArray)
+    {
+        int sumInt = 0, sumString = 0;
+
+        sumInt = objArray.Where(t => t is int).Sum(t => (int)t);
+        sumString = objArray.Where(t => t is string).Sum(t => Convert.ToInt32(t));
+
+        return sumInt - sumString;
+    }
+
+    public static void Main()
+    {
+        DivCon(new object[] { "5", "0", 9, 3, 2, 1, "9", 6, 7 });
+    }
+}
