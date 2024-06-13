ed9b923f607864d5ef224bf4812fd7d579158e74
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-06-13 12:23:35 +0200

    Exercise Difference of 2 [C#]

---
+++ Codewars/C#/Difference of 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static (int, int)[] TwosDifference(int[] array)
+    {
+        Array.Sort(array);
+
+        var result = new List<(int, int)>();
+
+        foreach (var number in array)
+        {
+            if (Array.IndexOf(array, number + 2) != -1)
+            {
+                result.Add((number, number + 2));
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    public static void Main()
+    {
+        var res = TwosDifference([15, 5, 3, 2]);
+
+        foreach (var number in res)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
