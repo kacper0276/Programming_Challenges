5b5aeca83daf8bf81b2f650a58815d236e2ba9fb
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-09 19:20:53 +0100

    Usuwanie (szkopuł) [C++]

---
+++ Usuwanie.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+long long count_even(long long a, long long b) {
+    return (b / 2) - ((a - 1) / 2);
+}
+
+long long count_odd(long long a, long long b) {
+    return (b - a + 1) - count_even(a, b);
+}
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+
+    long long evens = count_even(a, b);
+    long long odds = count_odd(a, b);
+
+    long long removed = (evens / 2) * 2 + (odds / 2) * 2;
+    cout << removed << endl;
+
+    return 0;
+}
