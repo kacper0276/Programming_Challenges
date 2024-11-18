10c09efa7ebd68fad2e2b22125f66b3400a034df
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-18 09:18:27 +0100

    H4x0r (Szkopuł) [C++]

---
+++ H4x0r.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string n;
+	cin >> n;
+
+	replace(n.begin(), n.end(), 'e', '3');
+	replace(n.begin(), n.end(), 'a', '4');
+	replace(n.begin(), n.end(), 'i', '1');
+	replace(n.begin(), n.end(), 'o', '0');
+	replace(n.begin(), n.end(), 's', '5');
+
+	cout << n;
+
+	return 0;
+}
