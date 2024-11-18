c0dafc12016c97acf80c47f4300391a4d2925bda
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-18 18:16:01 +0100

    Stopnie (Szkopuł) [C++]

---
+++ Stopnie.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double farenheitToCelcius(int n) {
+	return 5.0 / 9 * (n - 32);
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	cout << fixed << setprecision(2) << farenheitToCelcius(n);
+
+	return 0;
+}
