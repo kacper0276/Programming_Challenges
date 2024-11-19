13e358b72494c467320157565bb9ae7392a159cd
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-19 15:10:40 +0100

    Atak na mleczarnię (Szkopuł) [C++]

---
+++ Atak na mleczarnię.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_digits(string& str)
+{
+    for (char ch : str) {
+        int v = ch;
+        if (!(ch >= 48 && ch <= 57)) {
+			return false;
+        }
+    }
+
+	return true;
+}
+
+int main() {
+	int n, sum = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string chars;
+		cin >> chars;
+		
+		if (is_digits(chars)) {
+			sum += stoi(chars);
+		}
+	}
+
+	cout << sum;
+
+
+
+	return 0;
+}
