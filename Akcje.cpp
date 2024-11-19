638adccb957663614dbb43837db3771ae477d079
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-19 14:41:55 +0100

    Akcje (Szkopuł) [C++]

---
+++ Akcje.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> prices(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> prices[i];
+    }
+
+    int min_price = prices[0];
+    int max_profit = 0;
+
+    for (int i = 1; i < n; ++i) {
+        int profit = prices[i] - min_price;
+
+        max_profit = max(max_profit, profit);
+
+        min_price = min(min_price, prices[i]);
+    }
+
+    cout << max_profit << endl;
+    return 0;
+}
