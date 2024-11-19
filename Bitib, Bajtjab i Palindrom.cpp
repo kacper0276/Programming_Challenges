559bd7705dd84770252df6087c5369315a635d87
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-19 15:31:06 +0100

    Bitib, Bajtjab i Palindrom (Szkopuł) [C++]

---
+++ Bitib, Bajtjab i Palindrom.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+bool possible_build_palindrom(int* numbers, int n) {
+	map<int, int> counter;
+	bool odd = false;
+
+	for (int i = 0; i < n; i++) {
+		counter[numbers[i]]++;
+	}
+
+	for (auto it : counter) {
+		if (odd && it.second % 2 != 0) {
+			return false;
+		}
+		
+		if (!odd && it.second % 2 != 0) {
+			odd = true;
+		}
+	}
+
+	return true;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int* numbers = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> numbers[i];
+	}
+
+	if (possible_build_palindrom(numbers, n)) {
+		cout << "TAK";
+	}
+	else {
+		cout << "NIE";
+	}
+
+	return 0;
+}
