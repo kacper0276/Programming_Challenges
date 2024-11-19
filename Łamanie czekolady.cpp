b51368598bbad4de0259955c7a697ba8fed375a5
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-19 15:47:45 +0100

    Łamanie czekolady (Szkopuł) [C++]

---
+++ Łamanie czekolady.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	// n - ilość rzędów, m - ilość kolumn, k - ile kawałków czekolady
+	int n, m, k;
+
+	cin >> n >> m >> k;
+
+	if ((k % m == 0 && k / m <= n) || (k % n == 0 && k / n <= m)) {
+		cout << "TAK" << endl;
+	}
+	else {
+		cout << "NIE" << endl;
+	}
+
+	return 0;
+}
