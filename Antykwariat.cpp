858d6bd495cc57e2d0ad36c3b6612910ae8453c2
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-20 15:06:08 +0100

    Antykwariat (Szkopuł) [C++]

---
+++ Antykwariat.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+struct Book {
+	string title;
+	int pages;
+	int availability;
+};
+
+bool compareBooks(const Book& book1, const Book& book2) {
+	if (book1.availability != book2.availability) 
+		return book1.availability > book2.availability;
+	
+	if (book1.pages != book2.pages)
+		return book1.pages < book2.pages;
+
+	return book1.title < book2.title;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cin.ignore();
+
+	vector<Book> books;
+
+	for (int i = 0; i < n; i++) {
+		Book book;
+		getline(cin, book.title);
+		cin >> book.pages;
+		cin >> book.availability;
+		cin.ignore();
+		books.push_back(book);
+	}
+
+	sort(books.begin(), books.end(), compareBooks);
+
+	for (const auto& book : books) {
+		cout << book.availability << " " << book.title << " " << book.pages << endl;
+	}
+
+	return 0;
+}
