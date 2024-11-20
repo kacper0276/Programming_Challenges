83742bb78b877cf2f6a97030278d673d6dc49b68
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-20 15:08:43 +0100

    Przenoszenie (Szkopuł) [C++]

---
+++ Przenoszenie.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int countCarries(const string& num1, const string& num2) {
+	int carry = 0, carryCount = 0;
+	int n = num1.size(), m = num2.size();
+	int i = n - 1, j = m - 1;
+
+	while (i >= 0 || j >= 0 || carry > 0) {
+		int digitA = (i >= 0) ? num1[i--] - '0' : 0;
+		int digitB = (j >= 0) ? num2[j--] - '0' : 0;
+
+		int sum = digitA + digitB + carry;
+
+		if (sum >= 10) {
+			carry = 1;
+			carryCount++;
+		}
+		else {
+			carry = 0;
+		}
+	}
+
+	return carryCount;
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	while (n--) {
+		string num1, num2;
+		cin >> num1 >> num2;
+		cout << countCarries(num1, num2) << endl;
+	}
+
+	return 0;
+}
