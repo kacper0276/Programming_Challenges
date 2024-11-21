8c092c6f75f7b33aaa70130053a8dde1df6dabab
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-21 13:22:37 +0100

    Dwukrotnie wypisz tablicę 2 (Szkopuł) [C++]

---
+++ Dwukrotnie wypisz tablicę 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int* tab = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> tab[i];
+	}
+
+	cout << tab[1] << " " << tab[2] << " " << tab[3] << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << tab[i] << " ";
+	}
+
+	return 0;
+}
