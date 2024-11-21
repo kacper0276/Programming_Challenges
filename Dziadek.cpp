f65f085c3f01e8593de0cb73ef4a12b49bb5aecc
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-21 15:40:04 +0100

    Dziadek (Szkopuł) [C++]

---
+++ Dziadek.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countWord(string n) {
+	int j = 0;
+
+	for (int i = 0; i < n.length(); i += 3) {
+		if (n[i] == 'p')
+			j++;
+	}
+
+	return j;
+}
+
+int main() {
+	string n;
+	cin >> n;
+
+	cout << countWord(n) + 2;
+
+	return 0;
+}
