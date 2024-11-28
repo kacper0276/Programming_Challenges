4de8356904a68b2f4f8c16322d6b3b6b17032cd1
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-28 13:43:57 +0100

    Nauka ASCII (Szkopuł) [C++]

---
+++ Nauka ASCII.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	char s;
+	int w;
+
+	cin >> s >> w;
+
+	if (int(s) == w) {
+		cout << "Wow! - Az mi skarpetki spadly!";
+	}
+	else {
+		cout << "Ojc! - Nie ta melodia!";
+	}
+
+	return 0;
+}
