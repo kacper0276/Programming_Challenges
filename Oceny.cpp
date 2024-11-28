471a7f8f81c505b3881e73fc5935ff5cd22057c9
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-28 13:38:37 +0100

    Oceny (Szkopuł) [C++]

---
+++ Oceny.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int* marks = new int[n];
+	int res[7] = {0, 0, 0, 0, 0, 0, 0};
+
+	for (int i = 0; i < n; i++) {
+		cin >> marks[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		res[marks[i]]++;
+	}
+
+	for (int i = 1; i < 7; i++) {
+		cout << res[i] << " ";
+	}
+}
