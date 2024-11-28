f2856c31a0de86fef7063f68d2ce76f43e056a3f
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-28 13:28:17 +0100

    210 (Szkopuł) [C++]

---
+++ 210.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+string toBinary(int n) {
+    string binary = "";
+    while (n > 0) {
+        binary = (n % 2 == 0 ? "0" : "1") + binary;
+        n /= 2;
+    }
+    return (binary.empty() ? "0" : binary);
+}
+
+int sumBinaryDigits(const string& binary) {
+    int sum = 0;
+    for (char c : binary) {
+        sum += c - '0';
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    string binary = toBinary(n);
+
+    int sum = sumBinaryDigits(binary);
+
+    string binarySum = toBinary(sum);
+
+    cout << binary << " " << binarySum << endl;
+
+    return 0;
+}
