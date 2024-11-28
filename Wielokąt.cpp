6176ddc4e660628370be3d37812469ae55b42727
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-28 13:57:16 +0100

    Wielokąt (Szkopuł) [C++]

---
+++ Wielokąt.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sumArray(int* arr, int n) {
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+	}
+
+	return sum;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	cout << sumArray(arr, n);
+}
