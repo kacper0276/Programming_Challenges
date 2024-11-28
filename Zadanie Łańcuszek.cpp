a67a42385f057813e70f331622dec22c1663b862
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-11-28 13:50:38 +0100

    Zadanie Łańcuszek (Szkopuł) [C++]

---
+++ Zadanie Łańcuszek.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int phone_number, year_of_birth;
+
+	cin >> phone_number >> year_of_birth;
+
+	// Step 1
+	int res = (phone_number % 10) * 2;
+
+	// Step 2
+	res += 5;
+
+	// Step 3
+	res *= 50;
+
+	// Step 4
+	res += 1764;
+
+	// Step 5;
+	res -= year_of_birth;
+
+	cout << res;
+
+	return 0;
+}
