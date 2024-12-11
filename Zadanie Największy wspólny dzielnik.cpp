330d0fab0b1a724a144bcb70e50485a1725bf35f
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2024-12-11 14:32:57 +0100

    Zadanie Największy wspólny dzielnik (Szkopuł) [C++]

---
+++ Zadanie Największy wspólny dzielnik.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int NWD(int a, int b)
+{
+    while (a != b)
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    return a;
+}
+
+int main() {
+	int n1, n2;
+	cin >> n1 >> n2;
+
+    cout << NWD(n1, n2);
+
+	return 0;
+}
