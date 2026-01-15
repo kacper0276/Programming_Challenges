e1b6bc7a936c25c0ffd083e571458a7542a019b5
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-01-15 23:05:41 +0100

    [C++] Powitanie

---
+++ Powitanie.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << "Witaj na V Konkursie FRAKTAL! Suma " << a << " i " << b << " to " << a + b << "." << endl << "Powodzenia, programisto!";
+
+	return 0;
+}
