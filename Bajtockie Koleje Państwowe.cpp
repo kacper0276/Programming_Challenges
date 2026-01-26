8a35b2d9cc31daec6a3ba744d32e884bff5bda0e
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-01-26 23:24:54 +0100

    Bajtockie Koleje Państwowe [C++]

---
+++ Bajtockie Koleje Państwowe.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+
+    vector<int> degree(n + 1, 0);
+
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        degree[u]++;
+        degree[v]++;
+    }
+
+    long long needed_connections = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (degree[i] < 2) {
+            needed_connections += (static_cast<long long>(2) - degree[i]);
+        }
+    }
+
+    cout << needed_connections << "\n";
+
+    return 0;
+}
