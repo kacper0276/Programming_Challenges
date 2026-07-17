fa840ee71eac8395d957bc3091b26a5cd9155f2c
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-07-17 23:02:03 +0200

    Poprawne nawiasowanie (SPOJ) [C++]

---
+++ Poprawne nawiasowanie.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    if (cin >> s) {
+        long long n = s.length() / 2;
+
+        long long wynik = n * (n + 1) / 2;
+
+        cout << wynik << "\n";
+    }
+
+    return 0;
+}
