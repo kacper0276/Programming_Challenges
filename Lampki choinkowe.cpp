b1db05ab46e55d03a58231192ec635fe8d05c0c5
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-07-20 22:41:55 +0200

    Lampki choinkowe (SPOJ) [C++]

---
+++ Lampki choinkowe.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, k;
+    if (cin >> n >> m >> k) {
+        int s = n + m - k;
+        int x = s / 2;
+        x = min(x, n);
+        x = min(x, m);
+        cout << 2 * x << "\n";
+    }
+    return 0;
+}
