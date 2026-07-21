b235578305d7b1029890d3d7b579bc8734c8e3e5
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-07-21 22:18:40 +0200

    Trening biegowy (SPOJ) [C++]

---
+++ Trening biegowy.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int d;
+    if (cin >> d) {
+        for (int i = 0; i < d; ++i) {
+            int n;
+            int hh, mm, ss;
+            char c1, c2;
+
+            cin >> n >> hh >> c1 >> mm >> c2 >> ss;
+
+            int total_seconds = hh * 3600 + mm * 60 + ss;
+            int X = total_seconds * 1000;
+
+            int q = X / n;
+            int r = X % n;
+
+            if (2 * r >= n) {
+                q = q + 1;
+            }
+
+            int mins = q / 60;
+            int secs = q % 60;
+
+            if (secs < 10) {
+                cout << mins << ":0" << secs << "\n";
+            }
+            else {
+                cout << mins << ":" << secs << "\n";
+            }
+        }
+    }
+    return 0;
+}
