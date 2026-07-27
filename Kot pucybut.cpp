744f5c547130f709a65f20d353afa96c42308122
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-07-27 22:08:02 +0200

    Kot pucybut (SPOJ) [C++]

---
+++ Kot pucybut.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int a, pa, b, pb;
+    if (!(cin >> a >> pa >> b >> pb)) return;
+    int d = abs(pa - pb) - 1;
+    if (a > b) {
+        if (d % 3 != 0) {
+            cout << 1 << "\n";
+        }
+        else {
+            cout << 0 << "\n";
+        }
+    }
+    else {
+        if (d % 3 != 0) {
+            cout << 0 << "\n";
+        }
+        else {
+            cout << 1 << "\n";
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
