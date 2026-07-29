54f20838afe84393451c3a10f200a382a8590575
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-07-30 00:04:45 +0200

    0 czy 1 (SPOJ) [C++]

---
+++ 0 czy 1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+    long long num;
+    if (!(cin >> num)) return;
+
+    long long total_sum = 0;
+    long long current_term_parity = num % 2;
+    string op;
+
+    while (cin >> op) {
+        if (op == "=") {
+            break;
+        }
+        cin >> num;
+        if (op == "*") {
+            current_term_parity = (current_term_parity * (num % 2)) % 2;
+        }
+        else if (op == "+" || op == "-") {
+            total_sum = (total_sum + current_term_parity) % 2;
+            current_term_parity = num % 2;
+        }
+    }
+
+    total_sum = (total_sum + current_term_parity) % 2;
+
+    if (total_sum == 0) {
+        cout << "TAK\n";
+    }
+    else {
+        cout << "NIE\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    if (cin >> n) {
+        while (n--) {
+            solve();
+        }
+    }
+    return 0;
+}
