a52878c81d72ece53bd55f154e8c5a30cf5ca088
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-08-07 17:51:38 +0200

    Józio i Rózia Cukierki (SPOJ) [C++]

---
+++ Józio i Rózia Cukierki.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long nwd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            long long a, b, d;
+            cin >> a >> b >> d;
+            long long g = nwd(a, b);
+            cout << (a / g) << "/" << ((a + b) / g) << "\n";
+        }
+    }
+    return 0;
+}
