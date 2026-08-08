8b6b5ed6414dc72457603e910fb551e236af701d
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-08-08 20:43:07 +0200

    Silnia (SPOJ) [C++]

---
+++ Silnia.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void solve() {
+    long long n, z;
+    cin >> n >> z;
+
+    if (z > n) {
+        cout << 0 << "\n";
+        return;
+    }
+
+    long long p = 0;
+    long long temp = n;
+
+    while (temp >= z) {
+        temp /= z;
+        p += temp;
+    }
+
+    cout << p << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+
+    return 0;
+}
