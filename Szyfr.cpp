3c4ac9703a87d9c5ab5fa9f188edbfd086112e4e
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-08-26 20:34:54 +0200

    Szyfr (SPOJ) [C++]

---
+++ Szyfr.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (cin >> n) {
+        while (n--) {
+            string s;
+            cin >> s;
+            string out = "";
+            for (size_t i = 0; i + 4 < s.length(); i += 5) {
+                int val = 0;
+                for (int j = 0; j < 5; ++j) {
+                    val = (val << 1) | (s[i + j] - '0');
+                }
+                out += (char)('A' + val);
+            }
+            cout << out << "\n";
+        }
+    }
+    return 0;
+}
