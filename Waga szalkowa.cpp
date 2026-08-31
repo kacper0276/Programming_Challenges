331a2fba66feabe2243a9c15f99b56fc273f0f6a
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-08-31 21:42:11 +0200

    Waga szalkowa (SPOJ) [C++]

---
+++ Waga szalkowa.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    long long n;
+    if (!(cin >> n)) return;
+
+    long long original_n = n;
+    vector<long long> left_weights;
+    vector<long long> right_weights;
+
+    long long power = 1;
+    while (n > 0) {
+        long long rem = n % 3;
+        if (rem == 1) {
+            right_weights.push_back(power);
+            n /= 3;
+        }
+        else if (rem == 2) {
+            left_weights.push_back(power);
+            n = (n + 1) / 3;
+        }
+        else {
+            n /= 3;
+        }
+        power *= 3;
+    }
+
+    cout << original_n;
+    for (long long w : left_weights) {
+        cout << " " << w;
+    }
+    cout << " =";
+    for (long long w : right_weights) {
+        cout << " " << w;
+    }
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
