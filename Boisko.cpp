94f5484e89affabb34db2ba6f21b547191c9855f
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-09-01 18:20:50 +0200

    Boisko (SPOJ) [C++]

---
+++ Boisko.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long K, Z, N, P, X;
+    if (cin >> K >> Z >> N >> P >> X) {
+        long long rem = K - P * Z - (N - P) * X;
+        long long A = 0;
+        if (rem > 0) {
+            A = (rem + N - 1) / N;
+        }
+        long long B = A + X;
+        cout << A << " " << B << "\n";
+    }
+
+    return 0;
+}
