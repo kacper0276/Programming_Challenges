c39032b99929aeab50d9a03a5f58c8fd740b8f15
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-09-05 06:34:24 +0200

    Maksymilian Ostrowski (SPOJ) [C++]

---
+++ Maksymilian Ostrowski.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<int, int> zliczenia;
+    int x;
+
+    while (cin >> x) {
+        zliczenia[x]++;
+    }
+
+    for (auto para : zliczenia) {
+        if (para.second % 3 != 0) {
+            cout << para.first << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
