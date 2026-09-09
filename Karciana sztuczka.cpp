eedd598d2b35a43f26690cb30404a02785f1fd25
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-09-09 19:40:57 +0200

    Karciana sztuczka (SPOJ) [C++]

---
+++ Karciana sztuczka.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int d;
+    if (!(cin >> d)) return 0;
+
+    while (d--) {
+        int n;
+        cin >> n;
+
+        vector<int> q(2 * n);
+        int head = 0, tail = 0;
+
+        for (int i = 0; i < n; ++i) {
+            q[tail++] = i;
+        }
+
+        vector<int> ans(n);
+        for (int val = 1; val <= n; ++val) {
+            int idx = q[head++];
+            ans[idx] = val;
+            if (head < tail) {
+                q[tail++] = q[head++];
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            cout << ans[i] << (i + 1 == n ? "" : " ");
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
