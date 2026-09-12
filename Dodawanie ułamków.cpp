550c30deca0f50078948533e12dd48eae5b9d1c0
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-09-12 15:45:35 +0200

    Dodawanie ułamków (SPOJ) [C++]

---
+++ Dodawanie ułamków.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    unsigned long long a, b, c, d;
+    char s1, s2;
+
+    if (cin >> a >> s1 >> b >> c >> s2 >> d) {
+        unsigned long long g = gcd(b, d);
+        unsigned long long num = a * (d / g) + c * (b / g);
+        unsigned long long den = (b / g) * d;
+
+        unsigned long long final_gcd = gcd(num, den);
+        num /= final_gcd;
+        den /= final_gcd;
+
+        cout << a << "/" << b << " + " << c << "/" << d << " = " << num << "/" << den << endl;
+    }
+
+    return 0;
+}
