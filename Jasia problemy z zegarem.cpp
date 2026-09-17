7ef81341263361c35398255eb8ae122579f12b52
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2026-09-18 00:14:55 +0200

    Jasia problemy z zegarem (SPOJ) [C++]

---
+++ Jasia problemy z zegarem.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    string a, b;
+    if (!(cin >> a >> b)) return;
+    int h1 = stoi(a.substr(0, 2)), m1 = stoi(a.substr(3, 2));
+    int h2 = stoi(b.substr(0, 2)), m2 = stoi(b.substr(3, 2));
+
+    int start = h1 * 60 + m1;
+    int end = h2 * 60 + m2;
+
+    if (end < start) {
+        end += 1440;
+    }
+
+    int k_min = (11 * start + 719) / 720;
+    int k_max = (11 * end) / 720;
+
+    cout << max(0, k_max - k_min + 1) << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
